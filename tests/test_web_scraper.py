0552658828639fe8e675aee9e5760d1ce09dc95c
arsummers <aliyasummers1@gmail.com>
2020-06-05 16:57:40 -0700

    printing wiki page

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -1,6 +1,2 @@
 from web_scraper.web_scraper import *
 
-def test_hello():
-    expected = 'hello'
-    actual = hello()
-    assert expected == actual
--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -1,2 +1,17 @@
-def hello():
-    return 'hello'
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://en.wikipedia.org/wiki/Battle_of_the_Bulge"
+
+page = requests.get(url)
+
+print(page.content)
+
+def get_citation_count():
+    pass
+
+def get_citation_report():
+    pass
+
+def get_citation_heading():
+    pass
